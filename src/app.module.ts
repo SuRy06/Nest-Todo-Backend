59d0a664318bc0336c807f7ffc1d3e8ac6b6209e
Sudeep Chowdhury <codeingsury@gmail.com>
2026-01-30 13:50:58 +0530

    Graphql and Todos setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,7 +1,36 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  ObjectType,
+  Field,
+  ID,
+  registerEnumType,
+  GraphQLISODateTime,
+} from '@nestjs/graphql';
+
+export enum TodoStatus {
+  UNDONE = 'UNDONE',
+  STARTED = 'STARTED',
+  DONE = 'DONE',
+}
+
+registerEnumType(TodoStatus, {
+  name: 'TodoStatus',
+  description: 'Current status of a todo item',
+});
 
 @ObjectType()
 export class Todo {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field(() => TodoStatus)
+  status: TodoStatus;
+
+  @Field(() => GraphQLISODateTime)
+  createdAt: Date;
+
+  @Field(() => GraphQLISODateTime)
+  updatedAt: Date;
 }
